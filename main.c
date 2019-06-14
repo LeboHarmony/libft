8cc653cf262c3a0f0722d55c28291373e41f96ff
LeboHarmony <kebithil@gmail.com>
2019-06-14 16:23:00 +0200

    main.c

---
+++ main.c
@@ -0,0 +1,177 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkebethi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/12 07:39:14 by lkebethi          #+#    #+#             */
+/*   Updated: 2019/06/14 16:21:53 by lkebethi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+int			main()
+{
+// memset
+//	char s1[50] = "WeThinkCode_ is the future of Afrika Tech Ind.";
+//
+//	printf("%s", ft_memset(s1 + 12, '-', 8));
+
+// bzero
+//	char s1[50] = "WeThinkCode_ is the future of Afrika Tech Ind.";
+	
+//	printf("%s", ft_bzero(s1, 12));
+
+// memcpy
+//	char s1[10] = "Hello";
+//	char s2[10] = "World";
+//
+//	printf("Before copy: %s", s1);
+//	printf("After copy: %s", ft_memcpy(s1, s2, 5));
+	
+// memccpy
+//	char s1[10] = "Hello";
+//	char s2[10] = "World";
+
+//	printf("%s", ft_memccpy( s2, s1, 'H', 8));
+
+// memmove
+//	char s1[10] = "Lebohang";
+//	char s2[10] = "Harmony";
+
+//	printf("%s", ft_memmove(s1, s2, 3));
+
+// memchr
+//	char s[10] = "Lebohang";
+
+//	printf("Before: %s\n", s);
+//	printf("After: %s", ft_memchr(s, 'o', 8));
+
+// memcmp
+	
+//	printf("After: %d", ft_memcmp(s1, s2, 5));
+
+// memalloc
+
+//	char *str;
+
+//	str = (char *)ft_memalloc(15);
+//	printf("Address is: %s", str);
+
+
+// memdel
+
+// putchar
+
+//	char c = 'M';
+
+//	ft_putchar(c);
+
+// putstr
+
+//	char s[] = "Hello World!";
+
+//	ft_putstr(s);
+//	printf("%s", s);
+
+// putendl
+
+//	char str[] = "Lebo";
+//	printf("%s", str);
+
+// putchar_fd
+
+//	char s1 = 'L';
+
+//	ft_putchar_fd(s1, 1);
+
+// putstr_fd
+
+//	ft_putstr_fd(str, 1);
+
+// putendl_fd
+
+//	ft_putendl_fd(str, 1);
+//	printf("%c", s1);
+
+// putnbr
+
+//	ft_putnbr(10);
+
+// putnbr_fd
+
+//	ft_putnbr_fd(15, 1);
+
+// strlen
+
+//	printf("Size of string is: %zu", ft_strlen(s));
+
+// strdup
+
+//	printf("%s", ft_strdup(s));
+
+// strcpy
+
+//	char dst[5] = "Lebo";
+//	char src[10] = "Harmony";
+
+//	printf("Before copy: %s\n", dst);
+//	printf("After copy: %s", ft_strcpy(dst, src));
+
+// strncpy
+
+//	printf("%s\n", ft_strncpy(dst, src, 2));
+
+// strcat & strncat
+
+//	const char s1[10] = "Lebohang";
+//	const char s2[10] = "ng";
+	
+//	printf("%s", ft_strncat(s1, s2, 1));
+	
+// strlcat
+
+//	printf("%zu", ft_strlcat(dst, src, 5));
+
+// strchr
+
+//	char s[20] = "Coding is Life";
+
+//	printf("%s", ft_strchr(s, 'i'));
+
+// strrchr
+
+//	printf("%s", ft_strrchr(s, 'i'));
+
+// strstr & strnstr
+
+//	printf("%s", ft_strnstr(s1, s2, 8));
+
+// strcmp & strncmp
+
+//	  char s1[10] = "HelloD";
+//	  char s2[10] = "HelloF";
+
+//	  printf("%d", ft_strncmp(s1, s2, 7));
+
+// strnew
+
+	char s[10] = "Lebohang";
+
+//	printf("%s", ft_strnew(2));	
+
+// strdel
+
+//	printf("%s", ft_strdel(s));
+
+// strclr
+
+	printf("%s\n", ft_strclr(s));
+
+
+
+	return (0);
+}
